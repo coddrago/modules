f7f1d4e7421eade30b169394f2c035eb8ee99018
Who? <155328415+coddrago@users.noreply.github.com>
2024-02-09 17:15:05 +0700

    Update modlist.py

--- modlist.py
+++ modlist.py
@@ -10,8 +10,8 @@ class ModulesList(loader.Module):
     strings = {"name": "ModulesList", "example": "Модуль для личного использования."}
     strings_ru = {"example": "Модуль для быстрого доступа к каналам с модулями"}
 
-    @loader.command(alias="cmd", ru_doc="| Быстрый доступ к каналам с модулями ")
+    @loader.command(alias="moduleslist", ru_doc="| Быстрый доступ к каналам с модулями ")
     async def modlist(self, m: Message):
-        """quick access to channels with modules"""
+        """| quick access to channels with modules"""
         custom_text = "🌘 Community-made modules\n💭 @hikarimods\n🦋 @morisummermods\n💚 @nalinormods\n🤩 @AstroModules\n🌚 @vsecoder_m\n☺️ @mm_mods\n😈 @apodiktum_modules\n😇 @wilsonmods\n👑 @DorotoroMods\n✌️ @HikkaFTGmods\n🎈 @nercymods\n🎄 @hikka_mods\n☀️ @sqlmerr_m"
         await m.edit(custom_text)
