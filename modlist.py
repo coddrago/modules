b5d481ce189cdf6fd393c213c36b40b3f16a7564
Who? <155328415+coddrago@users.noreply.github.com>
2024-04-15 01:10:34 +0700

    Update modlist.py

--- modlist.py
+++ modlist.py
@@ -99,6 +99,14 @@ class ModulesList(loader.Module):
         """ | Official channel with modules"""
         await utils.answer(message, self.strings["officialChannels"])
 
+    @loader.command(alias="setoffmlist", ru_doc=" [Текст] | Поставить текст")
+    async def setoffmodlist(self, message):
+        """[Text] | Set text"""
+        reply = await message.get_reply_message()
+        await utils.answer(message, self.strings["setted"])
+        self.set("text", reply.text)
+        self._text = self.get("text", self.strings["officialChannels"])
+
     @loader.command(alias="setmlist", ru_doc=" [Текст] | Поставить текст")
     async def setmodlist(self, message):
         """[Text] | Set text"""
