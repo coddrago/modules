3a897a8f668ab2a5ff4cc93b049c2faa24092542
Who? <155328415+coddrago@users.noreply.github.com>
2024-08-02 04:40:02 +0700

    Update loli.py

--- loli.py
+++ loli.py
@@ -12,6 +12,11 @@
 import os
 import logging
 from .. import loader, utils
+import random
+import time
+import datetime
+from telethon import functions
+from telethon.tl.custom import Message
 
 logger = logging.getLogger("LoliHentai")
 
@@ -22,6 +27,7 @@ class lolihentai(loader.Module):
         "name": "LoliHentai",
         "loading_photo": "<emoji document_id=5215327832040811010>⏳</emoji> <b>loading your loli photo...</b>",
         "error_loading": "<b>Failed to get photos. Please unblock @ferganteusbot</b>",
+        "search": "<emoji document_id=5328311576736833844>🔴</emoji> loading your photo..."
     }
     
     async def lolicmd(self, message):
@@ -46,3 +52,23 @@ class lolihentai(loader.Module):
                 os.remove(phota)
                 
                 await message.delete()
+                
+    async def loliccmd(self, message: Message):
+         """-> to get your loli"""
+         await message.edit(self.strings("search"))
+         time.sleep(1)
+         chat = "hdjrkdjrkdkd"
+         result = await message.client(
+             functions.messages.GetHistoryRequest(
+                 peer=chat,
+                 offset_id=0,
+                 offset_date=datetime.datetime.now(),
+                 add_offset=random.choice(range(1, 101, 2)),
+                 limit=1,
+                 max_id=0,
+                 min_id=0,
+                 hash=0,
+             ),
+         )
+         await message.delete()
+         await message.client.send_file(message.to_id, result.messages[0].media)
