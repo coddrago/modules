dc76c7854c2d3ec1fc3f856e5053f442df825207
Who? <155328415+coddrago@users.noreply.github.com>
2024-12-22 21:30:12 +0700

    Update

---
+++ ascii_face.py
@@ -0,0 +1,38 @@
+# ---------------------------------------------------------------------------------
+#░█▀▄░▄▀▀▄░█▀▄░█▀▀▄░█▀▀▄░█▀▀▀░▄▀▀▄░░░█▀▄▀█
+#░█░░░█░░█░█░█░█▄▄▀░█▄▄█░█░▀▄░█░░█░░░█░▀░█
+#░▀▀▀░░▀▀░░▀▀░░▀░▀▀░▀░░▀░▀▀▀▀░░▀▀░░░░▀░░▒▀
+# Name: Ascii face
+# Description: random ascii face from utils
+# Author: @codrago_m
+# ---------------------------------------------------------------------------------
+# 🔒    Licensed under the GNU AGPLv3
+# 🌐 https://www.gnu.org/licenses/agpl-3.0.html
+# ---------------------------------------------------------------------------------
+# Author: @codrago
+# Commands: ascii
+# scope: hikka_only
+# meta developer: @codrago_m
+# ---------------------------------------------------------------------------------
+
+__version__ = (1, 0, 0)
+
+from .. import loader, utils
+
+@loader.tds
+class Ascii_face(loader.Module):
+    """Random Ascii Face from utils"""
+    strings = {
+    "name": "Ascii_Face",
+    "ascii_face": "<emoji document_id=5343719226450385808>😛</emoji> <b>Your random AsciiFace:</b> ",
+    }
+    
+    strings_ru = {
+    "ascii_face": "<emoji document_id=5343719226450385808>😛</emoji> <b>Ваш рандомный AsciiFace:</b> "
+
+    async def asciicmd(self, message):
+        """| Get random ascii face"""
+        
+        ascii_face = utils.ascii_face()
+        
+        await utils.answer(message, (self.strings["ascii_face"] + f"<code>{ascii_face}</code>"))
