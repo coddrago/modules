fb6e7c359ec89b01c441a909840e467886eeaf38
Who? <155328415+coddrago@users.noreply.github.com>
2025-02-03 04:12:19 +1000

    Update ascii_face.py

--- ascii_face.py
+++ ascii_face.py
@@ -14,7 +14,8 @@
 # scope: hikka_only
 # meta developer: @codrago_m
 # meta banner: https://mods.codrago.top/banners/ascii_face.png
-# meta pic: https://0x0.st/s/TGT42nesnQ5TwvixG8nFEQ/8KPa.webp ---------------------------------------------------------------------------------
+# meta pic: https://0x0.st/s/TGT42nesnQ5TwvixG8nFEQ/8KPa.webp
+# ---------------------------------------------------------------------------------
 
 __version__ = (1, 0, 0)
 
