c941f335671cfbd38fdbc5caec79cae66d5e40af
Who? <155328415+coddrago@users.noreply.github.com>
2025-02-03 04:36:25 +1000

    Update randomizer.py

--- randomizer.py
+++ randomizer.py
@@ -13,6 +13,8 @@
 # Commands: id, chatid, userid
 # scope: hikka_only
 # meta developer: @codrago_m
+# meta banner: https://mods.codrago.top/banners/randomizer.png
+# meta pic: https://0x0.st/s/de32M5VljJzzSJxSUjrqtA/8KPR.webp
 # ---------------------------------------------------------------------------------
 
 __version__ = (1, 0, 0)
