68f688c97bc62aa3d785609a8773ff382a461a06
Who? <155328415+coddrago@users.noreply.github.com>
2025-02-03 04:27:57 +1000

    Update pinterest.py

--- pinterest.py
+++ pinterest.py
@@ -13,6 +13,8 @@
 # Commands: pinterest
 # scope: hikka_only
 # meta developer: @codrago_m
+# meta banner: https://mods.codrago.top/banners/pinterest.png
+# meta pic: https://0x0.st/s/PRHLZJFYjFlRZEOaVcaopw/8KP6.webp
 # ---------------------------------------------------------------------------------
 
 __version__ = (1, 0, 0)
