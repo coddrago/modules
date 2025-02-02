80f46a4ade0527411f05dac0c36bab71671b1b3a
Who? <155328415+coddrago@users.noreply.github.com>
2025-02-03 04:06:24 +1000

    Update DoxTool.py

--- DoxTool.py
+++ DoxTool.py
@@ -13,7 +13,8 @@
 # Commands: gb, deanon
 # scope: hikka_only
 # meta developer: @codrago_m
-# ---------------------------------------------------------------------------------
+# meta banner: https://mods.codrago.top/banners/loli.png
+# meta pic: https://0x0.st/s/QeZGoQSDvJ2RILRMUOjzfg/8KPc.webp ---------------------------------------------------------------------------------
 
 __version__ = (1, 0, 0)
 
