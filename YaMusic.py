7703d57aa381e906ba57a988f71d37e15d5add4a
Codrago | Tosha C. <155328415+coddrago@users.noreply.github.com>
2026-03-22 04:17:10 +0700

    как давно здесь не канал?

--- YaMusic.py
+++ YaMusic.py
@@ -1,7 +1,7 @@
 __version__ = (3, 2, 0)
 # meta banner: https://raw.githubusercontent.com/kamekuro/hikka-mods/main/banners/yamusic.png
 # packurl: https://raw.githubusercontent.com/coddrago/modules/refs/heads/dev/translations/yamusic.yml
-# meta developer: @codrago
+# meta developer: @codrago_m
 # old meta dev: @kamekuro xuesos
 # scope: heroku_only
 # scope: heroku_min 2.0.0
